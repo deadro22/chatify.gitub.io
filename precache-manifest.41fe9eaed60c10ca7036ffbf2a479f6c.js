2b643af6d335c3c26a4c35063f3a610314f4ad37
deadro22 <44213280+deadro22@users.noreply.github.com>
2020-04-23 02:58:03 +0100

    Updates

---
+++ precache-manifest.41fe9eaed60c10ca7036ffbf2a479f6c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4c4ae56728b60122a4092ce4356e579",
+    "url": "/chatify.gitub.io/index.html"
+  },
+  {
+    "revision": "631f7970f54bd0959793",
+    "url": "/chatify.gitub.io/static/css/main.a9c30f5c.chunk.css"
+  },
+  {
+    "revision": "45744cbaee0a4146a899",
+    "url": "/chatify.gitub.io/static/js/2.b57d4d7d.chunk.js"
+  },
+  {
+    "revision": "c8bbcf4a821aebd790d5ccd2e7478960",
+    "url": "/chatify.gitub.io/static/js/2.b57d4d7d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "631f7970f54bd0959793",
+    "url": "/chatify.gitub.io/static/js/main.4198686c.chunk.js"
+  },
+  {
+    "revision": "5b4e15a7c09aae16e39f",
+    "url": "/chatify.gitub.io/static/js/runtime-main.ea8cb39d.js"
+  },
+  {
+    "revision": "6065203719447e8246b86d2d849821b4",
+    "url": "/chatify.gitub.io/static/media/fa-brands-400.60652037.woff"
+  },
+  {
+    "revision": "81e35852f1c74faa7c66b4762a109d8c",
+    "url": "/chatify.gitub.io/static/media/fa-brands-400.81e35852.woff2"
+  },
+  {
+    "revision": "99fb42ae2d1caeeef928b88e424b02ab",
+    "url": "/chatify.gitub.io/static/media/fa-brands-400.99fb42ae.eot"
+  },
+  {
+    "revision": "b1c71dc4022e5dd80f01fa23658cc003",
+    "url": "/chatify.gitub.io/static/media/fa-brands-400.b1c71dc4.svg"
+  },
+  {
+    "revision": "cd7b4e46cd06a6dcaf787a454c71fc02",
+    "url": "/chatify.gitub.io/static/media/fa-brands-400.cd7b4e46.ttf"
+  },
+  {
+    "revision": "0145959f8f52958cf24da0eb15b7cd38",
+    "url": "/chatify.gitub.io/static/media/fa-light-300.0145959f.woff"
+  },
+  {
+    "revision": "03e67a20f133f8e37095cc9f988e51a4",
+    "url": "/chatify.gitub.io/static/media/fa-light-300.03e67a20.ttf"
+  },
+  {
+    "revision": "2ef6d99e18876b039be154afdedabf01",
+    "url": "/chatify.gitub.io/static/media/fa-light-300.2ef6d99e.eot"
+  },
+  {
+    "revision": "dea6e2053ba43b37f5d29daf544a0973",
+    "url": "/chatify.gitub.io/static/media/fa-light-300.dea6e205.woff2"
+  },
+  {
+    "revision": "e935c1861b8602b2dc6c669c42cf83ac",
+    "url": "/chatify.gitub.io/static/media/fa-light-300.e935c186.svg"
+  },
+  {
+    "revision": "12c6ff700b23c72480819b939a40198c",
+    "url": "/chatify.gitub.io/static/media/fa-regular-400.12c6ff70.ttf"
+  },
+  {
+    "revision": "5b1536b4f63d819f8fa413917073926f",
+    "url": "/chatify.gitub.io/static/media/fa-regular-400.5b1536b4.svg"
+  },
+  {
+    "revision": "5f838fc8f38524d4b71adedfaddb2ede",
+    "url": "/chatify.gitub.io/static/media/fa-regular-400.5f838fc8.woff2"
+  },
+  {
+    "revision": "6e684aa45636f8af27fa3d6db3bcd246",
+    "url": "/chatify.gitub.io/static/media/fa-regular-400.6e684aa4.woff"
+  },
+  {
+    "revision": "759ed07eb34028d659e7e52934affebb",
+    "url": "/chatify.gitub.io/static/media/fa-regular-400.759ed07e.eot"
+  },
+  {
+    "revision": "006cf3b7c239a0b2845c25e7665b4fbb",
+    "url": "/chatify.gitub.io/static/media/fa-solid-900.006cf3b7.ttf"
+  },
+  {
+    "revision": "5cea46da0b5bdbea3a0503ec0f55a06e",
+    "url": "/chatify.gitub.io/static/media/fa-solid-900.5cea46da.woff"
+  },
+  {
+    "revision": "9760d404c933f832702cd784072b5925",
+    "url": "/chatify.gitub.io/static/media/fa-solid-900.9760d404.svg"
+  },
+  {
+    "revision": "ca3a3491ce9b0c4028f7bb97510dfc0b",
+    "url": "/chatify.gitub.io/static/media/fa-solid-900.ca3a3491.woff2"
+  },
+  {
+    "revision": "d92b387165a85a5861108075d1595357",
+    "url": "/chatify.gitub.io/static/media/fa-solid-900.d92b3871.eot"
+  }
+]);
